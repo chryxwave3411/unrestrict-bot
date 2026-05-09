4d91a20eb9066e99582ca352cd49b0fddca154be
chryxwave3411 <nomantaray@gmail.com>
2026-05-09 22:35:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
-app.run_polling()w
+app.run_polling()
